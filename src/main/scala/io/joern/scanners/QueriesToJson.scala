2534ca192f28c525f3e9f5d52de4baddf2d4b38e
Fabian Yamaguchi <fabs@shiftleft.io>
2021-01-01 18:05:09 +0100

    Generate and release database as json

---
+++ src/main/scala/io/joern/scanners/QueriesToJson.scala
@@ -0,0 +1,14 @@
+package io.joern.scanners
+
+import io.shiftleft.dataflowengineoss.queryengine.EngineContext
+import org.json4s._
+import org.json4s.native.Serialization
+import better.files.File
+
+object QueriesToJson extends App {
+  implicit val engineContext: EngineContext = null
+  implicit val formats: AnyRef with Formats = Serialization.formats(NoTypeHints)
+  val queryDb = new QueryDatabase(new JoernDefaultArgumentProvider())
+  File("querydb.json")
+    .write(Serialization.write(queryDb.allQueries))
+}
