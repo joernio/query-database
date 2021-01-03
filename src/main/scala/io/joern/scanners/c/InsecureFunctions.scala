ea76e1987809bc07ac60bda2f07043017fd375c2
Suchakra Sharma <suchakra@shiftleft.io>
2021-01-02 16:42:08 -0800

    Add more insecure string functions

--- src/main/scala/io/joern/scanners/c/InsecureFunctions.scala
+++ src/main/scala/io/joern/scanners/c/InsecureFunctions.scala
@@ -42,4 +42,68 @@ object InsecureFunctions extends QueryBundle {
     }
   )
 
+  @q
+  def scanfUsed(): Query = Query(
+    name = "call-to-scanf",
+    author = Crew.suchakra,
+    title = "Insecure function scanf() used",
+    description =
+      """
+        | Avoid scanf() function as it can lead to reads beyond buffer boundary and cause
+        | buffer overflows. A secure alternative is fgets().
+        |""".stripMargin,
+    score = 4, { cpg =>
+      cpg.call("scanf")
+    }
+  )
+
+  @q
+  def strcatUsed(): Query = Query(
+    name = "call-to-strcat",
+    author = Crew.suchakra,
+    title = "Insecure functions strcat() or strncat() used",
+    description =
+      """
+        | Avoid strcat() or strncat() functions. The can be used insucurely causing non
+        | null-termianted strings leading to buffer overflows or memory corruption. 
+        | A secure alternative is strcat_s().
+        |""".stripMargin,
+    score = 4, { cpg =>
+      cpg.call("(strcat|strncat)")
+    }
+  )
+
+  @q
+  def strcpyUsed(): Query = Query(
+    name = "call-to-strcpy",
+    author = Crew.suchakra,
+    title = "Insecure functions strcpy() or strncpy() used",
+    description =
+      """
+        | Avoid strcpy() or strncpy() function. strcpy() does not check buffer lengths. 
+        | A possible mitigation could be strncpy() which could prevent buffer overflows
+        | but does not null-termiante strings leading to memory corruption. A secure 
+        | alternative (on BSD) is strlcpy().
+        |""".stripMargin,
+    score = 4, { cpg =>
+      cpg.call("(strcpy|strncpy)")
+    }
+  )
+
+  @q
+  def strtokUsed(): Query = Query(
+    name = "call-to-strtok",
+    author = Crew.suchakra,
+    title = "Insecure function strtok() used",
+    description =
+      """
+        | Avoid strtok() function as it modifies the original string in place and appends
+        | a null character after each token. This makes the original string unsafe.
+        | Suggested alternative is strtok_r() with saveptr.
+        |""".stripMargin,
+    score = 4, { cpg =>
+      cpg.call("strtok")
+    }
+  )
+
 }
--- src/test/scala/io/joern/scanners/c/InsecureFunctionsTests.scala
+++ src/test/scala/io/joern/scanners/c/InsecureFunctionsTests.scala
@@ -42,6 +42,29 @@ class InsecureFunctionsTests extends Suite {
         snprintf(buffer, BUFF_SIZE, argv[2], 4242);
       }
 
+      int insecure_scanf() {
+        char name[12];
+        scanf("%s", name);
+        printf("Hello %s!\n", name);
+        return 0
+      }
+
+      int insecure_strncat_strncpy() {
+        char buf[BUF_SIZE];
+        strncpy(buf, default_value, BUF_SIZE); // remediation is (BUFF_SIZE - 1) 
+        strncat(buf, another_buffer, BUF_SIZE - strlen(buf)); // remediation is (BUFF_SIZE - strlen(buf) - 1)
+        return 0
+      }
+
+      int insecure_strtok() {
+        char *token;
+        char *path = getenv("PATH");
+        token = strtok(path, ":");
+        puts(token);
+        printf("PATH: %s\n", path); // original path string now has '/usr/bin\0' now and is insecure to use
+        return 0;
+      }
+
     """
 
   "find insecure gets() function usage" in {
@@ -61,6 +84,38 @@ class InsecureFunctionsTests extends Suite {
       .name
       .toSet
     results shouldBe Set("insecure_sprintf", "insecure_printf")
+  }
+
+  "find insecure scanf() function usage" in {
+    InsecureFunctions.scanfUsed()(cpg).map(_.evidence) match {
+      case List(List(expr: nodes.Expression)) =>
+        expr.method.name shouldBe "insecure_scanf"
+      case _ => fail
+    }
+  }
+
+  "find insecure strncat() function usage" in {
+    InsecureFunctions.strcatUsed()(cpg).map(_.evidence) match {
+      case List(List(expr: nodes.Expression)) =>
+        expr.method.name shouldBe "insecure_strncat_strncpy"
+      case _ => fail
+    }
+  }
+
+  "find insecure strncpy() function usage" in {
+    InsecureFunctions.strcpyUsed()(cpg).map(_.evidence) match {
+      case List(List(expr: nodes.Expression)) =>
+        expr.method.name shouldBe "insecure_strncat_strncpy"
+      case _ => fail
+    }
+  }
+
+  "find insecure strtok() function usage" in {
+    InsecureFunctions.strtokUsed()(cpg).map(_.evidence) match {
+      case List(List(expr: nodes.Expression)) =>
+        expr.method.name shouldBe "insecure_strtok"
+      case _ => fail
+    }
   }
 
 }
