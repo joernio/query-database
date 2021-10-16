4278fb7de210a8853945f4e5be39963692b23f66
Claudiu-Vlad Ursache <claudiu+hw@shiftleft.io>
2021-10-16 23:43:06 +0200

    Add MIPS test cases for single query

--- src/test/scala/io/joern/scanners/ghidra/UserInputIntoDangerousFunctionsTests.scala
+++ src/test/scala/io/joern/scanners/ghidra/UserInputIntoDangerousFunctionsTests.scala
@@ -17,17 +17,29 @@ class UserInputIntoDangerousFunctionsTests  extends GhidraQueryTestSuite {
       }
     }
 
-    "executed on CPG for binary call to `strcpy`, and call to `getenv`, but no dataflow between them" should {
-      "return an empty set of matched method names" in {
+    "executed on CPG for binary with call to `strcpy`, and call to `getenv`, but no dataflow between them" should {
+      "return an empty set of matched method names for a x86_64 binary" in {
         buildCpgForBin("buf2_neg.exe")
+        val results = methodNamesForMatchedPoints(query)
+        results shouldBe Set()
+      }
+
+      "return an empty set of matched method names for a MIPS binary" in {
+        buildCpgForBin("buf2_neg.mips.exe")
         val results = methodNamesForMatchedPoints(query)
         results shouldBe Set()
       }
     }
 
     "executed on CPG for binary with dataflow between `getenv` return value and `strcpy` source argument" should {
-      "find main function with data flow between getenv and strcpy" in {
+      "find main function with data flow between getenv and strcpy for a x86_64 binary" in {
         buildCpgForBin("buf2.exe")
+        val results = methodNamesForMatchedPoints(query)
+        results shouldBe Set("main")
+      }
+
+      "find main function with data flow between getenv and strcpy for a MIPS binary" in {
+        buildCpgForBin("buf2.mips.exe")
         val results = methodNamesForMatchedPoints(query)
         results shouldBe Set("main")
       }
